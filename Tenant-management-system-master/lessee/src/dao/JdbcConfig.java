11f924544123bff7e64672559f14da353f57bf83
A心在遠方 <62977989+958t@users.noreply.github.com>
2023-09-05 21:46:55 +0800

    租户管理系统基于Swing+MySQL

---
+++ Tenant-management-system-master/lessee/src/dao/JdbcConfig.java
@@ -0,0 +1,8 @@
+package dao;
+
+public interface JdbcConfig {
+    String DRIVER = "com.mysql.cj.jdbc.Driver";
+    String URL = "jdbc:mysql://localhost:3306/spms1";
+    String USERNAME = "root";
+    String PASSWORD = "123456";
+}
